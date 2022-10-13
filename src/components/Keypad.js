0a8d9bb3ee9f5f59ee62c48d8952532e0b0ae5c9
Keshav Bhati <keshavbhati2000@gmail.com>
2022-10-13 10:57:26 -0400

    commit tree 7e088373fc633a3f86b1b5b4453e492d3a836b7d

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    
+    function enterPass(){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={enterPass}/>
+        </div>
     )
 }
 
